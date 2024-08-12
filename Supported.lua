8338fab15895ea9fe1975cb6dc21e61e93b751b1
Zeuxtronic <zeuxtronic@gmail.com>
2024-08-12 11:04:20 +0300

    the supported games for zeouron

---
+++ Supported.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        ID = 6933626061,
+        GOTOID = 6933626061,
+        GameName = "Tlk Prison",
+        Script = function() 
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/voidy434343/Zeouron-Mainiatic/main/Tlk%20prison%20obf.txt'))()
+        end
+    },
+	{
+        ID = 6312903733,
+        GOTOID = 5561268850,
+        GameName = "Randomly Generated Droids",
+        Script = function() 
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/voidy434343/Zeouron-Mainiatic/main/RGD.lua'))()
+        end
+    }
+}
