6c597946e9d656a57149b7b0ae3a3919ec195edd
Zeuxtronic <zeuxtronic@gmail.com>
2024-10-27 08:18:08 +0200

    Add files via upload

--- CurrentUpdate.lua
+++ CurrentUpdate.lua
@@ -1,3 +1,21 @@
 Update 1.3
 
-TEST
+[+] Changed Images to be PNGS instead of RBXAssset.
+[+] Added the Startup menu you are on Currently.
+[+] Added Phone scaling to UI Library.
+[+] Recoded the UI Library.
+[+] New animations
+
+Improved the RGD script heavily:
+	[+] Added "Auto Box"
+ 	[+] Added "Auto Kill Shopkeepers"
+  	[+] Added "Autoclicker"
+   	[+] Added "AutoEquipBest"
+    [+] Added "AutoHeal"
+    [+] Added "Enable PotionUI"
+    [+] Added "SwordReach"
+    
+    [+] New Tab: "Render"
+    	[+] Added "Esp Enemies"
+    	[+] Added "Esp Allies"
+    	[+] Added "Esp Players"
